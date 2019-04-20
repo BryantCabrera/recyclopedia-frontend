373f8a79487d5e4b73cb4b73ab2939d7cc6e3ea4
Bryant <cabrera.bryant@gmail.com>
2019-04-20 16:01:54 -0700

    Bryant completes upload image functionality.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import TrashScan from './components/TrashScan/TrashScan';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <TrashScan />
+
                 <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
--- src/components/TrashScan/TrashScan.js
+++ src/components/TrashScan/TrashScan.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
 import FileBase from 'react-file-base64';
 import { storage } from '../../firebase-config';
-import DefaultImg from '/default-img.jpg';
+import DefaultImg from '../../assets/default-img.jpg';
 
 // base api url being used
 const API_URL = "http://localhost:4000";
@@ -38,28 +37,28 @@ class App extends Component {
 
         if (method === "multer") {
 
-        let imageFormObj = new FormData();
+            let imageFormObj = new FormData();
 
-        imageFormObj.append("imageName", "multer-image-" + Date.now());
-        imageFormObj.append("imageData", e.target.files[0]);
+            imageFormObj.append("imageName", "multer-image-" + Date.now());
+            imageFormObj.append("imageData", e.target.files[0]);
 
-        // stores a readable instance of 
-        // the image being uploaded using multer
-        this.setState({
-            multerImage: URL.createObjectURL(e.target.files[0])
-        });
-
-        axios.post(`${API_URL}/image/uploadmulter`, imageFormObj)
-            .then((data) => {
-            if (data.data.success) {
-                alert("Image has been successfully uploaded using multer");
-                this.setDefaultImage("multer");
-            }
-            })
-            .catch((err) => {
-            alert("Error while uploading image using multer");
-            this.setDefaultImage("multer");
+            // stores a readable instance of 
+            // the image being uploaded using multer
+            this.setState({
+                multerImage: URL.createObjectURL(e.target.files[0])
             });
+
+            axios.post(`${API_URL}/image/uploadmulter`, imageFormObj)
+                .then((data) => {
+                if (data.data.success) {
+                    alert("Image has been successfully uploaded using multer");
+                    this.setDefaultImage("multer");
+                }
+                })
+                .catch((err) => {
+                alert("Error while uploading image using multer");
+                this.setDefaultImage("multer");
+                });
         } else if (method === "firebase") {
         let currentImageName = "firebase-image-" + Date.now();
 
