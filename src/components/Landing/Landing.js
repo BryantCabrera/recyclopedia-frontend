f07d01f638cab7360e7ceb312fc8037c4188a89f
Kevin <k.cuison@gmail.com>
2019-04-21 15:35:53 -0700

    kevin adds styling

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -18,19 +18,31 @@ class Landing extends Component {
                 Get Started
             </button>
             </div>
-          {/* <img src={logo}></img> */}
+            {/* <img src={logo}></img> */}
+          </div>
+          <div class="top-panel"></div>
         </div>
-        <div class="top-panel"></div>
-        </div>
-        <div className="landing__features">
+        <div className="landing__demo--video">
+          <div className="landing__features">
             <div className="landing__trashscan--panel">
-                <div className="landing__features--demo">
-                    <h1>Demo</h1>
-                    <TrashScan />
-                </div>
+              <div className="landing__features--demo">
+                <h1>Demo</h1>
+                <TrashScan />
+              </div>
             </div>
-        </div>
-        {/* <div className="landing_features-both">
+            <div className="landing__video">
+
+            {/* <video id="bgvid" style={{ width: '100%' }}
+                src="https://www.youtube.com/embed/iTEsuURdDck"
+                loop
+                autoPlay
+                muted /> */}
+
+                {/* <iframe width="auto" height="550" src="https://www.youtube.com/embed/iTEsuURdDck" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+
+            </div>
+          </div>
+          {/* <div className="landing_features-both">
           <div className="landing__features--incentives">
             <h1>Incentives</h1>
           </div>
@@ -38,13 +50,14 @@ class Landing extends Component {
             <h1>Impact</h1>
           </div>
         </div> */}
-        <div className="landing__demo--description">
-          <div className="landing__demo--panel">
-            <div className="landing__demo--description--text">
-              <h2>Demo Description</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt id tenetur beatae dolores corporis eaque.</p>
+          {/* <div className="landing__demo--description">
+            <div className="landing__demo--panel">
+              <div className="landing__demo--description--text">
+                <h2>Demo Description</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt id tenetur beatae dolores corporis eaque.</p>
+              </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     )
